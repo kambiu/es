a14732ab016fcf1d8cd4454dc8feecd7a4f63f7a
= <=>
2017-12-04 20:25:18 +0800

    udpatupdate Setup

--- BSetupIndexMapping.py
+++ BSetupIndexMapping.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch
 
-
+print()
 es = Elasticsearch(["127.0.0.1:9200"])
 
 
@@ -8,77 +8,74 @@ es = Elasticsearch(["127.0.0.1:9200"])
 
 indices_templates = {
     # index #1
-    "school": {
+    "student": {
         "index_settings": {
             "number_of_shards": 1,
             "number_of_replicas": 1
         },
-        # same field name in same index but different type cannot have different analyzer, eg. date_modified
-        # aka in one index, one field name only one mapping can be applied
-        "type_mappings": {
-            # type 1 of index #1
-            "student": {
-                "properties": {
-                    "date_created": {
-                        "type": "date",
-                        "format": "epoch_millis"
-                    },
-                    "date_modified": {
-                        "type": "date",
-                        "format": "yyyy-MM-dd HH:mm:ss||dd/MM/yyyy HH:mm:ss"
-                    },
-                    "age": {"type": "integer"},
-                    "height": {"type": "float"},
-                    "courses": {"type": "keyword"},
-                    "grade": {"type": "keyword"},
-                    "content": {"type": "text"},
-                    "active": {"type": "boolean"}
-                }
-            },
-            # type 2 of index #1
-            "staff": {
-                "properties": {
-                    "date_created": {
-                        "type": "date",
-                        "format": "epoch_millis"
-                    },
-                    "date_modified": {
-                        "type": "date",
-                        "format": "yyyy-MM-dd HH:mm:ss||dd/MM/yyyy HH:mm:ss"
-                    },
-                    "age": {"type": "integer"},
-                    "height": {"type": "float"},
-                    "courses": {"type": "keyword"},
-                    "content": {"type": "text"},
-                    "active": {"type": "boolean"}
-                }
+        "student": {
+            "properties": {
+                "date_created": {
+                    "type": "date",
+                    "format": "epoch_millis"
+                },
+                "date_modified": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm:ss||dd/MM/yyyy HH:mm:ss"
+                },
+                "age": {"type": "integer"},
+                "height": {"type": "float"},
+                "courses": {"type": "keyword"},
+                "grade": {"type": "keyword"},
+                "content": {"type": "text"},
+                "active": {"type": "boolean"}
+            }
+        }
+    },
+    "staff": {
+        "index_settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 1
+        },
+        "staff": {
+            "properties": {
+                "date_created": {
+                    "type": "date",
+                    "format": "epoch_millis"
+                },
+                "date_modified": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm:ss||dd/MM/yyyy HH:mm:ss"
+                },
+                "age": {"type": "integer"},
+                "height": {"type": "float"},
+                "courses": {"type": "keyword"},
+                "content": {"type": "text"},
+                "active": {"type": "boolean"}
             }
         }
     },
     # index #2
-    "backery": {
+    "cake": {
         "index_settings": {
             "number_of_shards": 1,
             "number_of_replicas" : 1
         },
-        "type_mappings": {
-            # type 1 of index #2
-            "cake": {
-                "properties": {
-                    "date_created": {
-                        "type": "date",
-                        "format": "epoch_millis"
-                    },
-                    "date_expired": {
-                        "type": "date",
-                        "format": "yyyy/MM/dd"
-                    },
-                    "price": {"type": "float"},
-                    "weight": {"type": "float"},
-                    "grade": {"type": "keyword"},
-                    "content": {"type": "text"},
-                    "active": {"type": "boolean"}
-                }
+        "cake": {
+            "properties": {
+                "date_created": {
+                    "type": "date",
+                    "format": "epoch_millis"
+                },
+                "date_expired": {
+                    "type": "date",
+                    "format": "yyyy/MM/dd"
+                },
+                "price": {"type": "float"},
+                "weight": {"type": "float"},
+                "grade": {"type": "keyword"},
+                "content": {"type": "text"},
+                "active": {"type": "boolean"}
             }
         }
     },
@@ -88,12 +85,9 @@ indices_templates = {
             "number_of_shards": 1,
             "number_of_replicas" : 1
         },
-        "type_mappings": {
-            # type 1 of index #2
-            "autocomplete": {
-                "properties": {
-                    "suggest": {"type": "completion"}
-                }
+        "words": {
+            "properties": {
+                "suggest": {"type": "completion"}
             }
         }
     }
@@ -107,11 +101,7 @@ def create_index_mapping():
         # create index with shards & replica
         es.indices.delete(index=index_name, ignore=([404]))
         es.indices.create(index=index_name, body=config["index_settings"])
-
-        # create mapping for each types in the index creating
-        for type, prop in config["type_mappings"].items():
-            print("{} {}".format(type, prop))
-            es.indices.put_mapping(index=index_name, doc_type=type, body=prop)
+        es.indices.put_mapping(index=index_name, doc_type=index_name, body=config[index_name])
 
 
 def main():
